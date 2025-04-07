8b6359160a7602b29f3732da94d78e31a7b4448f
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-04-06 23:38:02 -0600

    Part 2 done

--- index.js
+++ index.js
@@ -37,17 +37,40 @@ async function getUserDataAsunc(id) {
     };
 
     try {
+        let vaultDataPr = vault(id); //starting immediately because it is not dependent on the db
         let dbWithData = await central(id); // 100ms to get the needed db
         let userDataPr = dbs[dbWithData](id); //this is a promise
-        let vaultDataPr = vault(id); //this is another promise
+        //let vaultDataPr = vault(id); //this is another promise 
+        //by movin vaultDataPr BEFORE await central(id) they can run in parralel and in saves about 100ms
         let [userData, vaultData] = await Promise.all([userDataPr, vaultDataPr]); // running both priomisses in parralel
         //console.log(userData, vaultData);
         return combineUserData(id, userData, vaultData);
     } catch (error) {
         throw error; // propagating the error        
     }
-
 }
+
+//this is a promise realisation
+function getUserDataPromise(id) {
+    const dbs = {
+        db1: db1,
+        db2: db2,
+        db3: db3
+    };
+    let dbWithData = central(id);
+    return dbWithData.then((db) => {
+        let userDataPr = dbs[db](id);
+        let vaultDataPr = vault(id);
+        return Promise.all([userDataPr, vaultDataPr]).then(([userData, vaultData]) => {
+            return combineUserData(id, userData, vaultData);
+        }).catch(err => {
+            throw err; //propagating the error
+        })
+    }).catch(err => {
+        throw err; //propagating the error
+    });
+}
+
 
 
 // first async approach
@@ -56,24 +79,32 @@ try {
     const userDataAsync1 = getUserDataAsunc(1); // this is a promise we should await
     const userDataAsync2 = getUserDataAsunc(2); // this is a promise we should await
     const userDataAsync3 = getUserDataAsunc(3); // this is a promise we should await
-    console.log(await Promise.all([userDataAsync1,userDataAsync2,userDataAsync3])); // awaiting 3 promisses in parralel
+    console.log(await Promise.all([userDataAsync1, userDataAsync2, userDataAsync3])); // awaiting 3 promisses in parralel
 } catch (error) {
     console.error(error.message);
 }
 const endTime = performance.now(); //ending timer
-console.log(`Async/await time: ${endTime - startTime} ms`); 
+console.log(`Async/await time: ${endTime - startTime} ms`);
 
 
-// // second promise approach
-// const startTime2 = performance.now(); // stating timer
-// try {
-//     const userDataAsync1 = getUserDataAsunc(1); // this is a promise we should await
-//     const userDataAsync2 = getUserDataAsunc(2); // this is a promise we should await
-//     const userDataAsync3 = getUserDataAsunc(3); // this is a promise we should await
-//     console.log(await Promise.all([userDataAsync1,userDataAsync2,userDataAsync3])); // awaiting 3 promisses in parralel
-// } catch (error) {
-//     console.error(error.message);
-// }
-// const endTime2 = performance.now(); //ending timer
-// console.log(`Promise time: ${endTime2 - startTime2} ms`); 
+// second promise approach
+const startTime2 = performance.now(); // stating timer
+try {
+    const userDataPromise4 = getUserDataPromise(4); // this is a promise 
+    const userDataPromise5 = getUserDataPromise(5); // this is a promise 
+    const userDataPromise6 = getUserDataPromise(6); // this is a promise 
+    Promise.all([userDataPromise4, userDataPromise5, userDataPromise6])
+        .then(data => {
+            console.log(data);
+            const endTime2 = performance.now(); //ending timer
+            console.log(`Promise time: ${endTime2 - startTime2} ms`);
+        }).catch(err => {
+            console.error(err.message)
+        });
+
+    //userDataPromise1.then(date => console.log(date)); // awaiting 3 promisses in parralel       
+} catch (error) {
+    console.error(error.message);
+}
+
 
